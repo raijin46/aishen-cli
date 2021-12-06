08da1ae38834d9e1a928fd7582eaa145f3390910
aishen <stanyui166@gmail.com>
2021-12-06 14:52:12 +0800

    initialize app instance
    - added a colored logging message
    - set up enviroment variables

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,11 @@
 #! /usr/bin/env node
-console.log( process.argv.slice( 2 ) );
+import App from '../src/interfaces/app.js';
+import Message from '../src/interfaces/message.js';
+import dotenv from 'dotenv';
+
+// dotenv.config();
+Message.system( 'Initializing interface...' );
+const app = new App();
+app.setup()
+  .then( () => Message.system( 'App is done setting up!' ) )
+  .catch( e => Message.system( `Error occured.\n${ e }`, 'red' ) );
